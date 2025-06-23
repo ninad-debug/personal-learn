e6b09f925a1a6bf3a49fcd06e91d5e85812ff2fc
ninad-debug <ninadsamant07@email.com>
2025-06-23 19:18:19 +0530

    first commit

---
+++ namaste-js/index_5.js
@@ -0,0 +1,11 @@
+// SHORTEST JS Program 🔥window & this keyword | Namaste JavaScript Ep. 5
+
+var a = 10;
+
+function b() {
+    var x= 10;
+}
+
+console.log(a);
+console.log(window.a);
+console.log(this.a);
---
+++ namaste-js/index_6.js
@@ -0,0 +1,20 @@
+// undefined vs not defined in JS 🤔 | Namaste JavaScript Ep. 6 
+
+var a;
+
+console.log(a);
+
+a = 10;
+
+if (a === undefined) {
+    console.log("a is undefined");
+} else {
+    console.log("a is defined");
+}
+
+var b;
+console.log(b);
+b = 20;
+console.log(b);
+b = "Namaste JS";
+console.log(b);
